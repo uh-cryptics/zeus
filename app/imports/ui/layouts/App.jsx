0df00d14c256ee32a36702323c5b92a105aea55f
deshay-clemons <54691651+deshay-clemons@users.noreply.github.com>
2021-09-03 14:06:43 -1000

    #1 Rough Draft to List Inventory Page
    - Redirects to Inventory after Signin/up
    - Need to make one more fix for it to be complete with displaying more complex sample object data.

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -15,6 +15,7 @@ import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
 import Signout from '../pages/Signout';
 import Homepage from '../pages/Homepage';
+import Inventory from '../pages/Inventory';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
@@ -27,6 +28,7 @@ class App extends React.Component {
               <Route path="/signin" component={Signin}/>
               <Route path="/signup" component={Signup}/>
               <ProtectedRoute path="/homepage" component={Homepage}/>
+              <ProtectedRoute path="/inventory" component={Inventory}/>
               <ProtectedRoute path="/list" component={ListStuff}/>
               <ProtectedRoute path="/add" component={AddStuff}/>
               <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
