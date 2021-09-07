2d5ea39226d332179ad422aee751e1ac4f03710a
rabitz808 <77714922+rabitz808@users.noreply.github.com>
2021-09-07 12:42:00 -1000

    Changed inventory page to component

--- app/imports/ui/pages/Homepage.jsx
+++ app/imports/ui/pages/Homepage.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { Segment } from 'semantic-ui-react';
-import NavBar from '../components/NavBar';
-import MedicationTab from '../components/MedicationTab';
+import ListInventory from '../components/ListInventory';
 
 /** A simple static component to render some text for the landing page. */
 class Homepage extends React.Component {
   render() {
     return (
         <div>
-          <NavBar/>
-        <Segment color="black" inverted basic>
-          <MedicationTab></MedicationTab>
-        </Segment>
+        <ListInventory/>
         </div>
     );
   }
