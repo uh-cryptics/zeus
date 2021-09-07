571b7a051320a1f7b97f3337f64b928ceebbcedb
rabitz808 <77714922+rabitz808@users.noreply.github.com>
2021-09-07 13:04:02 -1000

    Updated navbar, homepage, and inventory page

--- app/imports/ui/pages/Homepage.jsx
+++ app/imports/ui/pages/Homepage.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import ListInventory from '../components/ListInventory';
 
 /** A simple static component to render some text for the landing page. */
 class Homepage extends React.Component {
   render() {
     return (
         <div>
-        <ListInventory/>
+        <h1>Work in progress</h1>
         </div>
     );
   }
