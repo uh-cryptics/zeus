961bd9847512a58a0ae5044d3b08c5bf3be72cd7
rabitz808 <77714922+rabitz808@users.noreply.github.com>
2021-09-07 12:15:55 -1000

    Update inventory theme
    Changed container to segment

--- app/imports/ui/pages/Inventory.jsx
+++ app/imports/ui/pages/Inventory.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Segment } from 'semantic-ui-react';
 import NavBar from '../components/NavBar';
 
 /** A simple static component to render some text for the landing page. */
@@ -11,388 +11,387 @@ class Inventory extends React.Component {
   }
 
   handleTable(e) {
-    e = e == 'true' ? true : false;
-    
+    e = e == 'true';
+
     if (this.state.medicationTable != e) {
-      this.setState({medicationTable: e});
+      this.setState({ medicationTable: e });
     }
   }
 
   render() {
-    /** 
+    /**
      * These two varaibles (supplies and medication sample) will be removed once backend
-     * database is set, just getting a feel how things would look on the page. 
+     * database is set, just getting a feel how things would look on the page.
      */
-    let suppliesSample = [
+    const suppliesSample = [
       {
-        "name": "Theraband - Green",
-        "location": "Cabinet 2",
-        "amount": 1,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'Theraband - Green',
+        location: 'Cabinet 2',
+        amount: 1,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "Theraband - Yellow",
-        "location": "Cabinet 2",
-        "amount": 1,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'Theraband - Yellow',
+        location: 'Cabinet 2',
+        amount: 1,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "Theraband - Red",
-        "location": "Cabinet 2",
-        "amount": 1,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'Theraband - Red',
+        location: 'Cabinet 2',
+        amount: 1,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "Theraband - Beige",
-        "location": "Cabinet 2",
-        "amount": 1,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'Theraband - Beige',
+        location: 'Cabinet 2',
+        amount: 1,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "Hot Packs",
-        "location": "Cabinet 2",
-        "amount": 0,
-        "reserves_location": "Back Cabinet",
-        "reserves_amount": 0
+        name: 'Hot Packs',
+        location: 'Cabinet 2',
+        amount: 0,
+        reserves_location: 'Back Cabinet',
+        reserves_amount: 0,
       },
       {
-        "name": "Cold Packs",
-        "location": "Cabinet 2",
-        "amount": 0,
-        "reserves_location": "Back Cabinet",
-        "reserves_amount": 0
+        name: 'Cold Packs',
+        location: 'Cabinet 2',
+        amount: 0,
+        reserves_location: 'Back Cabinet',
+        reserves_amount: 0,
       },
       {
-        "name": "ACE wrap 2\"",
-        "location": "",
-        "amount": 0,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'ACE wrap 2"',
+        location: '',
+        amount: 0,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "ACE wrap 3\"",
-        "location": "",
-        "amount": 0,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'ACE wrap 3"',
+        location: '',
+        amount: 0,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "ACE wrap 4\"",
-        "location": "",
-        "amount": 0,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'ACE wrap 4"',
+        location: '',
+        amount: 0,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "ACE wrap 6\"",
-        "location": "",
-        "amount": 0,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'ACE wrap 6"',
+        location: '',
+        amount: 0,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "Blood Pressure Monitors - Patient",
-        "location": "Back Cabinet",
-        "amount": 3,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'Blood Pressure Monitors - Patient',
+        location: 'Back Cabinet',
+        amount: 3,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "Cavicide",
-        "location": "Shower Closet",
-        "amount": [
+        name: 'Cavicide',
+        location: 'Shower Closet',
+        amount: [
           1,
-          "Gal"
+          'Gal',
         ],
-        "reserves_location": "",
-        "reserves_amount": 0
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "Rapid Strep Strips",
-        "location": "Refrig Closet",
-        "amount": 25,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'Rapid Strep Strips',
+        location: 'Refrig Closet',
+        amount: 25,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "Emesis Basin",
-        "location": "Drawer 9",
-        "amount": 0,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'Emesis Basin',
+        location: 'Drawer 9',
+        amount: 0,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "Hand Sanitizer - Large",
-        "location": "",
-        "amount": 0,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'Hand Sanitizer - Large',
+        location: '',
+        amount: 0,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "Hand Sanitizer - Small for Patients",
-        "location": "",
-        "amount": 0,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'Hand Sanitizer - Small for Patients',
+        location: '',
+        amount: 0,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "Pregnancy Tests",
-        "location": "Refrigerator",
-        "amount": 20,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'Pregnancy Tests',
+        location: 'Refrigerator',
+        amount: 20,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "Urine Dip Sticks",
-        "location": "",
-        "amount": 0,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'Urine Dip Sticks',
+        location: '',
+        amount: 0,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "Sensi Wrap 2\"",
-        "location": "Drawer 6",
-        "amount": 6,
-        "reserves_location": "Back Cabinet",
-        "reserves_amount": 30
+        name: 'Sensi Wrap 2"',
+        location: 'Drawer 6',
+        amount: 6,
+        reserves_location: 'Back Cabinet',
+        reserves_amount: 30,
       },
       {
-        "name": "UnaBoot",
-        "location": "",
-        "amount": 0,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'UnaBoot',
+        location: '',
+        amount: 0,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "Thermometers - Patient Supplies",
-        "location": "Case 4",
-        "amount": 3,
-        "reserves_location": "",
-        "reserves_amount": 0
+        name: 'Thermometers - Patient Supplies',
+        location: 'Case 4',
+        amount: 3,
+        reserves_location: '',
+        reserves_amount: 0,
       },
       {
-        "name": "Thermometer Sheaths",
-        "location": "Case 4",
-        "amount": 2,
-        "reserves_location": "",
-        "reserves_amount": 0
-      }
+        name: 'Thermometer Sheaths',
+        location: 'Case 4',
+        amount: 2,
+        reserves_location: '',
+        reserves_amount: 0,
+      },
     ];
 
-    let medicationSample = [
+    const medicationSample = [
       {
-        "name": "Benzonatate Capsules",
-        "type": "Allergy & Cold Medicines",
-        "location": "Case 2",
-        "amount": 15,
-        "supply": [
+        name: 'Benzonatate Capsules',
+        type: 'Allergy & Cold Medicines',
+        location: 'Case 2',
+        amount: 15,
+        supply: [
           19,
-          100
+          100,
         ],
-        "expiration": [
-          "10/16",
-          "3/17"
+        expiration: [
+          '10/16',
+          '3/17',
         ],
-        "reserves_supply": 0,
-        "reserves_expiration": "03/17/2021"
+        reserves_supply: 0,
+        reserves_expiration: '03/17/2021',
       },
       {
-        "name": "Cyclobenzaprine 10 mg",
-        "type": "Analgesics/Antiinflammatory",
-        "location": "Case 4",
-        "amount": 30,
-        "supply": 80,
-        "expiration": "03/17/2021",
-        "reserves_supply": 0,
-        "reserves_expiration": null
+        name: 'Cyclobenzaprine 10 mg',
+        type: 'Analgesics/Antiinflammatory',
+        location: 'Case 4',
+        amount: 30,
+        supply: 80,
+        expiration: '03/17/2021',
+        reserves_supply: 0,
+        reserves_expiration: null,
       },
       {
-        "name": "Gabapentin 300 mg ",
-        "type": "Analgesics/Antiinflammatory",
-        "location": "Case 4",
-        "amount": 30,
-        "supply": 112,
-        "expiration": [
-          "6/16",
-          "3/17"
+        name: 'Gabapentin 300 mg ',
+        type: 'Analgesics/Antiinflammatory',
+        location: 'Case 4',
+        amount: 30,
+        supply: 112,
+        expiration: [
+          '6/16',
+          '3/17',
         ],
-        "reserves_supply": 0,
-        "reserves_expiration": null
+        reserves_supply: 0,
+        reserves_expiration: null,
       },
       {
-        "name": "Lidocaine w/o Epi, injectable",
-        "type": "Analgesics/Antiinflammatory",
-        "location": "Bottom Drawer",
-        "amount": [
+        name: 'Lidocaine w/o Epi, injectable',
+        type: 'Analgesics/Antiinflammatory',
+        location: 'Bottom Drawer',
+        amount: [
           1,
-          "bottle"
+          'bottle',
         ],
-        "supply": 2,
-        "expiration": "10/16/2021",
-        "reserves_supply": 0,
-        "reserves_expiration": null
+        supply: 2,
+        expiration: '10/16/2021',
+        reserves_supply: 0,
+        reserves_expiration: null,
       },
       {
-        "name": "Prednisone 20 mg tabs",
-        "type": "Analgesics/Antiinflammatory",
-        "location": "Case 4",
-        "amount": 20,
-        "supply": 70,
-        "expiration": "3/1/17",
-        "reserves_supply": 0,
-        "reserves_expiration": null
+        name: 'Prednisone 20 mg tabs',
+        type: 'Analgesics/Antiinflammatory',
+        location: 'Case 4',
+        amount: 20,
+        supply: 70,
+        expiration: '3/1/17',
+        reserves_supply: 0,
+        reserves_expiration: null,
       },
       {
-        "name": "Tramadol 50 mg Tabs",
-        "type": "Analgesics/Antiinflammatory",
-        "location": "Case 4",
-        "amount": 50,
-        "supply": 500,
-        "expiration": "04/17/2021",
-        "reserves_supply": 0,
-        "reserves_expiration": null
+        name: 'Tramadol 50 mg Tabs',
+        type: 'Analgesics/Antiinflammatory',
+        location: 'Case 4',
+        amount: 50,
+        supply: 500,
+        expiration: '04/17/2021',
+        reserves_supply: 0,
+        reserves_expiration: null,
       },
       {
-        "name": "Amlodipine 5 mg ",
-        "type": "Antihypertensives",
-        "location": "Case 7",
-        "amount": 30,
-        "supply": 32,
-        "expiration": "4/30/18",
-        "reserves_supply": 0,
-        "reserves_expiration": null
+        name: 'Amlodipine 5 mg ',
+        type: 'Antihypertensives',
+        location: 'Case 7',
+        amount: 30,
+        supply: 32,
+        expiration: '4/30/18',
+        reserves_supply: 0,
+        reserves_expiration: null,
       },
       {
-        "name": "Amlodipine 10 mg",
-        "type": "Antihypertensives",
-        "location": "Case 7",
-        "amount": 30,
-        "supply": [
+        name: 'Amlodipine 10 mg',
+        type: 'Antihypertensives',
+        location: 'Case 7',
+        amount: 30,
+        supply: [
           57,
-          46
+          46,
         ],
-        "expiration": [
-          "4/16",
-          "4/30/2018"
+        expiration: [
+          '4/16',
+          '4/30/2018',
         ],
-        "reserves_supply": 0,
-        "reserves_expiration": null
+        reserves_supply: 0,
+        reserves_expiration: null,
       },
       {
-        "name": "Amlodipine 5mg/ Atorvastatin 20 mg",
-        "type": "Antihypertensives",
-        "location": "Case 7",
-        "amount": 0,
-        "supply": 84,
-        "expiration": [
-          "8/16",
-          "8/17"
+        name: 'Amlodipine 5mg/ Atorvastatin 20 mg',
+        type: 'Antihypertensives',
+        location: 'Case 7',
+        amount: 0,
+        supply: 84,
+        expiration: [
+          '8/16',
+          '8/17',
         ],
-        "reserves_supply": 0,
-        "reserves_expiration": null
+        reserves_supply: 0,
+        reserves_expiration: null,
       },
       {
-        "name": "Chlorthalidone or Hydrochlorothiazide 25 mg tabs",
-        "type": "Antihypertensives",
-        "location": "Case 7",
-        "amount": 50,
-        "supply": [
+        name: 'Chlorthalidone or Hydrochlorothiazide 25 mg tabs',
+        type: 'Antihypertensives',
+        location: 'Case 7',
+        amount: 50,
+        supply: [
           85,
-          134
+          134,
         ],
-        "expiration": [
-          "6/16",
-          "9/16"
+        expiration: [
+          '6/16',
+          '9/16',
         ],
-        "reserves_supply": 0,
-        "reserves_expiration": null
+        reserves_supply: 0,
+        reserves_expiration: null,
       },
       {
-        "name": "Furosemide 20 mg ",
-        "type": "Antihypertensives",
-        "location": "Case 7",
-        "amount": 30,
-        "supply": 86,
-        "expiration": "4/1/16",
-        "reserves_supply": 0,
-        "reserves_expiration": null
+        name: 'Furosemide 20 mg ',
+        type: 'Antihypertensives',
+        location: 'Case 7',
+        amount: 30,
+        supply: 86,
+        expiration: '4/1/16',
+        reserves_supply: 0,
+        reserves_expiration: null,
       },
       {
-        "name": "Lisinopril 10 mg tab",
-        "type": "Antihypertensives",
-        "location": "Case 7",
-        "amount": 30,
-        "supply": 0,
-        "expiration": null,
-        "reserves_supply": 0,
-        "reserves_expiration": null
+        name: 'Lisinopril 10 mg tab',
+        type: 'Antihypertensives',
+        location: 'Case 7',
+        amount: 30,
+        supply: 0,
+        expiration: null,
+        reserves_supply: 0,
+        reserves_expiration: null,
       },
       {
-        "name": "Azilsartan/chlorthalidone 40mg/25mg",
-        "type": "Antihypertensives",
-        "location": "Case 7",
-        "amount": 0,
-        "supply": 70,
-        "expiration": "7/1/16",
-        "reserves_supply": {
-          "quantity": 2,
-          "unit": "bottle"
+        name: 'Azilsartan/chlorthalidone 40mg/25mg',
+        type: 'Antihypertensives',
+        location: 'Case 7',
+        amount: 0,
+        supply: 70,
+        expiration: '7/1/16',
+        reserves_supply: {
+          quantity: 2,
+          unit: 'bottle',
         },
-        "reserves_expiration": null
+        reserves_expiration: null,
       },
     ];
 
     /** Grabs header information */
     function tableHeader(data) {
-      const headers = Object.keys(data[0])
+      const headers = Object.keys(data[0]);
       const tableHeader = headers.map((header, i) => {
-        const key = `h${i}`
-        header = header.toUpperCase()
+        const key = `h${i}`;
+        header = header.toUpperCase();
 
-        const modifiedHeader = header.replace(/_/g, ' ')
+        const modifiedHeader = header.replace(/_/g, ' ');
         return (
           <th key={key}>{modifiedHeader}</th>
-        )
-      })
-      return (tableHeader)
+        );
+      });
+      return (tableHeader);
     }
 
     /** Grabs the values of each column data */
     function tableData(data) {
-      const headers = Object.keys(data[0])
+      const headers = Object.keys(data[0]);
 
       const listedItems = data.map((row, i) => {
         const columns = (Object.values(row)).map((col, j) => {
-          const key = `${i}_${j}`
-
+          const key = `${i}_${j}`;
 
           if (typeof (col) === 'object' && col != null) {
-            col = Object.values(col).join(' ')
+            col = Object.values(col).join(' ');
           }
           return (
             <td key={key} data-label={headers[j]}>{col}</td>
-          )
-        })
+          );
+        });
 
         return (
           <tr key={i}>
             {columns}
           </tr>
-        )
-      })
+        );
+      });
 
-      return (listedItems)
+      return (listedItems);
     }
 
     return (
       <div>
         <NavBar />
-        <Container fluid className="background-blue padding-homepage">
-          <Container fluid textAlign="center" className="inventory-background-black">
+        <Segment inverted basic color='blue'>
+          <Segment inverted basic textAlign='center'>
             <h1 className="fontsize-big h1-white">Inventory</h1>
             {this.state.medicationTable ?
               <div>
@@ -433,8 +432,8 @@ class Inventory extends React.Component {
                 </div>
               </div>
             }
-          </Container>
-        </Container>
+          </Segment>
+        </Segment>
       </div >
     );
   }
